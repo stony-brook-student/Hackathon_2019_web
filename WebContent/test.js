f3b3d167849417cff91562a556ffbfaec7a1bc10
runzyang <runzhe.yang@qq.com>
2019-02-16 00:25:05 -0500

    Web project created

---
+++ WebContent/test.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+
+ 
+
+function myFunction() {
+	document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function stepscondition() {
+	var element = document.getElementById("step1");   
+    if(element.style.display=="none")  {
+    	element.style.display="";  
+    }else{
+    	element.style.display="none";   
+    }    
+}
+
