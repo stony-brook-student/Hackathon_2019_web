33baec346f9be28e7fd2a110f4e1c16ca2d59064
DESKTOP-84SKG63\Frost <yingnan.li@stonybrook.edu>
2019-02-17 08:31:13 -0500

    update yifan's name

--- WebContent/MainPage.js
+++ WebContent/MainPage.js
@@ -13,19 +13,19 @@ function uploadFile(files) {
     var req = new XMLHttpRequest();
     var url = "https://www.googleapis.com/upload/storage/v1/b/hackathon2019/o?uploadType=media&name="+files[0].name;
     req.open("POST",url);
-    req.setRequestHeader("Authorization", "Bearer ya29.GluzBuZk2VmtxDv9Dqo_fnAA_9_FYaOy5LT-mzuFL99e4j907yzTWw0S6qHVD-8loreR-X_cQh87Hi1jTfdwVATo3FQYsZyS3cXQUq5_RVOQz3v-7UvRZXPOBQ-O");
+    req.setRequestHeader("Authorization", "Bearer ya29.GlyzBmBoYan8h1ZJTHXFqhBnh7ohNeSh2QMNUtWRyp23zh-wU3N_SInWIsGw1KhwkwJOEk1llNY0qDHhCA5EvECeaiqcDmO7Mg4OPWKsxopQHCsDkcU_Z13qrHMHyw");
     req.setRequestHeader("Content-Type", files[0].type);
     var blob = new Blob(files);
     req.send(blob);
     req.onreadystatechange = function () {
         if(this.readyState==4 && this.status==200){
             var json = JSON.parse(req.responseText);
-            console.log(json);
+            console.log(req.responseText);
             // send the  file  name to your python
             url = "http://34.73.56.231/python/hackathon.py";
             req.open("POST",url);
             var json = JSON.parse(req.responseText);
-            console.log(json);
+            console.log(req.responseText);
         }
     };
 
